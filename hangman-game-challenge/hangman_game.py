333116aabdc02784fcecb1afada13069484961ca
Rodof <rodfloresp@gmail.com>
2022-05-31 22:23:26 -0400

    a function who replace de wowels whit acent whit the same letter without acent

--- hangman-game-challenge/hangman_game.py
+++ hangman-game-challenge/hangman_game.py
@@ -1,3 +1,4 @@
+from ctypes.wintypes import WORD
 from curses.ascii import isalpha
 import os
 from pickle import NONE #To clear screen
@@ -89,9 +90,20 @@ def get_word():
         random_word = random.choice(words.split()) 
     return random_word.upper()
 
+#a function who replace the wowels of random_word with acent with the same letter  (e.g. á -> a)
+def replace():
+    word = get_word()
+    word = word.replace("Á", "A")
+    word = word.replace("É", "E")
+    word = word.replace("Í", "I")
+    word = word.replace("Ó", "O")
+    word = word.replace("Ú", "U")
+    return word
 
-def play(random_word):
-    word_completion = "_" * len(random_word)
+
+
+def play(word):
+    word_completion = "_" * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
@@ -106,7 +118,7 @@ def play(random_word):
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print("You already guessed the letter", guess)
-            elif guess not in random_word:
+            elif guess not in word:
                 print(guess, "is not in the word.")
                 tries -= 1
                 guessed_letters.append(guess)
@@ -114,22 +126,22 @@ def play(random_word):
                 print("Good job,", guess, "is in the word!")
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
-                indices = [i for i, letter in enumerate(random_word) if letter == guess]
+                indices = [i for i, letter in enumerate(word) if letter == guess]
                 for index in indices:
                     word_as_list[index] = guess
                 word_completion= "".join(word_as_list)
                 if "_" not in word_completion:
                     guessed = True
-        elif len(guess) == len(random_word) and guess.isalpha():
+        elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
                 print("You already guessed the word", guess)
-            elif guess != random_word:
+            elif guess != word:
                 print(guess, "is not the word.")
                 tries -= 1
                 guessed_words.append(guess)
             else:
                 guessed = True
-                word_completion = random_word
+                word_completion = word
         else:
             print("Not a valid guess.")
         clear()
@@ -140,11 +152,12 @@ def play(random_word):
     if guessed:
         print("Congrats, you guessed the word! You win")
     else:
-        print("Sorry, you ran out of tries. The word was " + random_word + ". Maybe next time!")
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
 
 def run():
     get_word()
-    play(get_word()) 
+    replace()
+    play(replace())
 
 
 if __name__ == '__main__':
